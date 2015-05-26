792608bf2d7ce0c3bd3f380369e90ba351319e31
Alberto Donato <alberto.donato@gmail.com>
2015-05-26 21:27:34 +0300

    Cleanup config.py

--- sshoot/config.py
+++ sshoot/config.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with sshoot.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Handle configuration files."""
+'''Handle configuration files.'''
 
 import yaml
 import os
@@ -22,23 +22,23 @@ from sshoot.profile import Profile
 
 
 def yaml_dump(data, fh=None):
-    """Dump data in YAML format with sane defaults for readability."""
+    '''Dump data in YAML format with sane defaults for readability.'''
     return yaml.safe_dump(
         data, fh, default_flow_style=False, allow_unicode=True)
 
 
 class Config(object):
-    """Handle configuration file loading/saving."""
+    '''Handle configuration file loading/saving.'''
 
-    CONFIG_KEYS = frozenset(["executable"])
+    CONFIG_KEYS = frozenset(['executable'])
 
     def __init__(self, path):
-        self._config_file = os.path.join(path, "config.yaml")
-        self._profiles_file = os.path.join(path, "profiles.yaml")
+        self._config_file = os.path.join(path, 'config.yaml')
+        self._profiles_file = os.path.join(path, 'profiles.yaml')
         self._reset()
 
     def load(self):
-        """Load configuration from file."""
+        '''Load configuration from file.'''
         self._reset()
         self._config = self._load_yaml_file(self._config_file)
         profiles = self._load_yaml_file(self._profiles_file)
@@ -46,39 +46,39 @@ class Config(object):
             self._profiles[name] = Profile.from_dict(self._from_config(conf))
 
     def save(self):
-        """Save profiles configuration to file."""
-        with open(self._profiles_file, "w") as fh:
+        '''Save profiles configuration to file.'''
+        with open(self._profiles_file, 'w') as fh:
             yaml_dump(self._build_profiles_config(), fh)
 
     def add_profile(self, name, profile):
-        """Add a profile to the configuration."""
+        '''Add a profile to the configuration.'''
         if name in self._profiles:
             raise KeyError(name)
         self._profiles[name] = profile
 
     def remove_profile(self, name):
-        """Add the given profile to the configuration."""
+        '''Add the given profile to the configuration.'''
         del self._profiles[name]
 
     @property
     def profiles(self):
-        """Return a dict with profiles, using names as key."""
+        '''Return a dict with profiles, using names as key.'''
         return self._profiles.copy()
 
     @property
     def config(self):
-        """Return a dict with the configuration."""
+        '''Return a dict with the configuration.'''
         return {
             key: value for key, value in self._config.items()
             if key in self.CONFIG_KEYS}
 
     def _reset(self):
-        """Reset default empty config."""
+        '''Reset default empty config.'''
         self._profiles = {}
         self._config = {}
 
     def _load_yaml_file(self, path):
-        """Load the specified YAML file."""
+        '''Load the specified YAML file.'''
         if not os.path.exists(path):
             return {}
 
@@ -87,19 +87,19 @@ class Config(object):
             return yaml.load(fh) or {}
 
     def _build_profiles_config(self):
-        """Return the profiles config dict to be saved to file."""
+        '''Return the profiles config dict to be saved to file.'''
         return {
             name: self._to_config(profile.config())
             for name, profile in self._profiles.items()}
 
     def _from_config(self, config):
-        """Convert a config to a params dict."""
+        '''Convert a config to a params dict.'''
         return {
-            key.replace("-", "_"): value
+            key.replace('-', '_'): value
             for key, value in config.items()}
 
     def _to_config(self, params):
-        """Convert a params dict to a config."""
+        '''Convert a params dict to a config.'''
         return {
-            key.replace("_", "-"): value
+            key.replace('_', '-'): value
             for key, value in params.items()}
--- sshoot/tests/test_config.py
+++ sshoot/tests/test_config.py
@@ -30,15 +30,15 @@ class YamlDumpTests(TestCase):
 
     def setUp(self):
         super(YamlDumpTests, self).setUp()
-        self.data = {"foo": "bar", "baz": [1, 2]}
+        self.data = {'foo': 'bar', 'baz': [1, 2]}
 
     def test_dump_to_string(self):
-        """The method returns YAML data as a string by default."""
+        '''The method returns YAML data as a string by default.'''
         result = yaml_dump(self.data)
         self.assertEqual(yaml.load(stream=StringIO(result)), self.data)
 
     def test_dump_to_file(self):
-        """The method dumps YAML data to the specified file."""
+        '''The method dumps YAML data to the specified file.'''
         fh = StringIO()
         result = yaml_dump(self.data, fh=fh)
         fh.seek(0)
@@ -51,74 +51,74 @@ class ConfigTests(TestWithFixtures):
     def setUp(self):
         super(ConfigTests, self).setUp()
         self.tempdir = self.useFixture(TempDir()).path
-        self.config_path = os.path.join(self.tempdir, "config.yaml")
-        self.profiles_path = os.path.join(self.tempdir, "profiles.yaml")
+        self.config_path = os.path.join(self.tempdir, 'config.yaml')
+        self.profiles_path = os.path.join(self.tempdir, 'profiles.yaml')
         self.config = Config(self.tempdir)
 
     def test_add_profile(self):
-        """Profiles can be added to the config."""
+        '''Profiles can be added to the config.'''
         profiles = {
-            "profile1": Profile(["10.0.0.0/24"]),
-            "profile2": Profile(["192.168.0.0/16"])}
+            'profile1': Profile(['10.0.0.0/24']),
+            'profile2': Profile(['192.168.0.0/16'])}
         for name, profile in profiles.items():
             self.config.add_profile(name, profile)
         self.assertEqual(self.config.profiles, profiles)
 
     def test_add_profile_name_present(self):
-        """An exception is raised if the profile name is already used."""
-        self.config.add_profile("profile", Profile(["10.0.0.0/24"]))
+        '''An exception is raised if the profile name is already used.'''
+        self.config.add_profile('profile', Profile(['10.0.0.0/24']))
         self.assertRaises(
-            KeyError, self.config.add_profile, "profile",
-            Profile(["192.168.0.0/16"]))
+            KeyError, self.config.add_profile, 'profile',
+            Profile(['192.168.0.0/16']))
 
     def test_remove_profile(self):
-        """Profiles can be removed to the config."""
+        '''Profiles can be removed to the config.'''
         profiles = {
-            "profile1": Profile(["10.0.0.0/24"]),
-            "profile2": Profile(["192.168.0.0/16"])}
+            'profile1': Profile(['10.0.0.0/24']),
+            'profile2': Profile(['192.168.0.0/16'])}
         for name, profile in profiles.items():
             self.config.add_profile(name, profile)
-        self.config.remove_profile("profile1")
-        self.assertCountEqual(["profile2"], self.config.profiles.keys())
+        self.config.remove_profile('profile1')
+        self.assertCountEqual(self.config.profiles.keys(), ['profile2'])
 
     def test_remove_profile_not_present(self):
-        """An exception is raised if the profile name is not known."""
-        self.assertRaises(KeyError, self.config.remove_profile, "profile")
+        '''An exception is raised if the profile name is not known.'''
+        self.assertRaises(KeyError, self.config.remove_profile, 'profile')
 
     def test_load_from_file(self):
-        """The config is loaded from file."""
+        '''The config is loaded from file.'''
         profiles = {
-            "profile": {
-                "subnets": ["10.0.0.0/24"],
-                "auto-nets": True}}
-        with open(self.profiles_path, "w") as fh:
+            'profile': {
+                'subnets': ['10.0.0.0/24'],
+                'auto-nets': True}}
+        with open(self.profiles_path, 'w') as fh:
             yaml.dump(profiles, stream=fh)
         self.config.load()
-        profile = self.config.profiles["profile"]
-        self.assertEqual(profile.subnets, ["10.0.0.0/24"])
+        profile = self.config.profiles['profile']
+        self.assertEqual(profile.subnets, ['10.0.0.0/24'])
         self.assertTrue(profile.auto_nets)
 
     def test_load_missing(self):
-        """If no config files are found, config is empty."""
+        '''If no config files are found, config is empty.'''
         self.config.load()
         self.assertEqual(self.config.profiles, {})
         self.assertEqual(self.config.config, {})
 
     def test_load_config_options(self):
-        """Only known config options are loaded from config file."""
-        config = {"executable": "/usr/bin/shuttle", "other-conf": "no"}
-        with open(self.config_path, "w") as fh:
+        '''Only known config options are loaded from config file.'''
+        config = {'executable': '/usr/bin/shuttle', 'other-conf': 'no'}
+        with open(self.config_path, 'w') as fh:
             yaml.dump(config, stream=fh)
         self.config.load()
         self.assertEqual(
-            self.config.config, {"executable": "/usr/bin/shuttle"})
+            self.config.config, {'executable': '/usr/bin/shuttle'})
 
     def test_load_profiles(self):
-        """The "profiles" config field is loaded from the config file."""
+        '''The 'profiles' config field is loaded from the config file.'''
         profiles = {
-            "profile1": {"subnets": ["10.0.0.0/24"]},
-            "profile2": {"subnets": ["192.168.0.0/16"]}}
-        with open(self.profiles_path, "w") as fh:
+            'profile1': {'subnets': ['10.0.0.0/24']},
+            'profile2': {'subnets': ['192.168.0.0/16']}}
+        with open(self.profiles_path, 'w') as fh:
             yaml.dump(profiles, stream=fh)
         self.config.load()
         expected = {
@@ -127,10 +127,10 @@ class ConfigTests(TestWithFixtures):
         self.assertEqual(self.config.profiles, expected)
 
     def test_save_profiles(self):
-        """Profiles are saved to file."""
+        '''Profiles are saved to file.'''
         profiles = {
-            "profile1": {"subnets": ["10.0.0.0/24"], "remote": "hostname1"},
-            "profile2": {"subnets": ["192.168.0.0/16"], "remote": "hostname2"}}
+            'profile1': {'subnets': ['10.0.0.0/24'], 'remote': 'hostname1'},
+            'profile2': {'subnets': ['192.168.0.0/16'], 'remote': 'hostname2'}}
         self.config.load()
         for name, conf in profiles.items():
             self.config.add_profile(name, Profile.from_dict(conf))
@@ -140,18 +140,18 @@ class ConfigTests(TestWithFixtures):
         self.assertEqual(config, profiles)
 
     def test_save_from_file(self):
-        """The config is saved to file."""
-        conf = {"subnets": ["10.0.0.0/24"], "auto_nets": True}
-        self.config.add_profile("profile", Profile.from_dict(conf))
+        '''The config is saved to file.'''
+        conf = {'subnets': ['10.0.0.0/24'], 'auto_nets': True}
+        self.config.add_profile('profile', Profile.from_dict(conf))
         self.config.save()
 
         config = dedent(
-            """\
+            '''\
             profile:
               auto-nets: true
               subnets:
               - 10.0.0.0/24
-            """)
+            ''')
         with open(self.profiles_path) as fh:
             content = fh.read()
         self.assertEqual(content, config)
