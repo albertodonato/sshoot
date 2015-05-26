cbb2872ac86956ff2519093c9756ebdec0d201a8
Alberto Donato <alberto.donato@gmail.com>
2015-05-26 21:18:04 +0300

    Cleanups

--- sshoot/__init__.py
+++ sshoot/__init__.py
@@ -1,19 +1,19 @@
 #
 # This file is part of sshoot.
-
+#
 # sshoot is free software: you can redistribute it and/or modify it under the
 # terms of the GNU General Public License as published by the Free Software
 # Foundation, either version 3 of the License, or (at your option) any later
 # version.
-
+#
 # sshoot is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 # A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with sshoot.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Tool for handling ssshuttle VPN sessions."""
+'''Tool for handling ssshuttle VPN sessions.'''
 
 
-__version__ = "1.0.3"
+__version__ = '1.0.3'
