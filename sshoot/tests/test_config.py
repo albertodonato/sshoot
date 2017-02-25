72a84bea97a69e5eb3d3ec137a9b80a51c99fc17
Alberto Donato <alberto.donato@gmail.com>
2017-02-25 20:53:05 +0100

    Remove headers.

--- sshoot/tests/test_config.py
+++ sshoot/tests/test_config.py
@@ -1,23 +1,7 @@
-#
-# This file is part of sshoot.
-#
-# sshoot is free software: you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
-#
-# sshoot is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with sshoot.  If not, see <http://www.gnu.org/licenses/>.
-
 import os
 from io import StringIO
 import yaml
 from textwrap import dedent
-
 from unittest import TestCase
 
 from fixtures import TempDir, TestWithFixtures
