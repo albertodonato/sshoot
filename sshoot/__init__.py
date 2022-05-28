4993d3e98f138448c91ba36a90c1f366ce0ea15e
Alberto Donato <alberto.donato@gmail.com>
2022-05-28 09:18:51 +0200

    replace distutils

--- sshoot/__init__.py
+++ sshoot/__init__.py
@@ -1,9 +1,11 @@
 """Manage multiple sshuttle VPN sessions."""
 
-from distutils.version import LooseVersion
+from packaging.version import Version
 
 import pkg_resources
 
-__all__ = ["__version__"]
+__all__ = ["PACKAGE", "__version__"]
 
-__version__ = LooseVersion(pkg_resources.require("sshoot")[0].version)
+PACKAGE = pkg_resources.get_distribution("sshoot")
+
+__version__ = Version(PACKAGE.version)
